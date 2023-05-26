5e2b4242752b0e58081c5ce2389b8961602d1a57
Iker S.P <ikersierra5@gmail.com>
2023-05-26 16:49:41 +0200

    Modified login view and editing view

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,73 +1,91 @@
- import { Component, NgModule, OnInit } from '@angular/core';
- import { InputTextModule } from 'primeng/inputtext';
- import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
- import { UserService } from 'src/app/services/user.service';
- import { Router } from '@angular/router';
- import { ToastModule } from 'primeng/toast';
- import { MessageService } from 'primeng/api';
+import { Component, NgModule, OnInit } from '@angular/core';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { UserService } from 'src/app/services/user.service';
+import { Router } from '@angular/router';
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
+import { TextBoxModule, } from '@syncfusion/ej2-angular-inputs';
+import { CommonModule } from '@angular/common';
+import { PasswordModule } from 'primeng/password';
+import { Validators } from '@angular/forms';
 
- @Component({
-   selector: 'app-login',
-   templateUrl: './login.component.html',
-   styleUrls: ['./login.component.css'],
-   providers: [MessageService]
- })
- export class LoginComponent implements OnInit {
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+  providers: [MessageService]
+})
+export class LoginComponent implements OnInit {
 
-   formLogin: FormGroup;
+  formLogin: FormGroup;
 
-   constructor(
-     private userService: UserService,
-     private router: Router,
-     private messageService: MessageService
-   ) {
-     this.formLogin = new FormGroup({
-       email: new FormControl(),
-       password: new FormControl()
-     })
-   }
+  constructor(
+    private userService: UserService,
+    private router: Router,
+    private messageService: MessageService
+  ) {
+    this.formLogin = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required])
+    })
+  }
 
-   ngOnInit(): void {
-   }
-   show() {
-     this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Usuario incorrecto' });
-   }
-   onSubmit() {
+  ngOnInit(): void {
+  }
+  show() {
+    this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Usuario incorrecto' });
+  }
+  onSubmit() {
 
-     let loading = document.getElementById("spinner");
-     let loginButton = document.getElementById("loginButton");
-     loading!.hidden = false;
-     loginButton!.setAttribute("disabled", "disabled");
-     this.userService.login(this.formLogin.value)
-       .then(response => {
-         setTimeout(() => {
-           this.router.navigate(['/main'])
-           loading!.hidden = true;
-         }, 1000)
-       })
-     setTimeout(() => {
-       loading!.hidden = true;
-       loginButton?.removeAttribute("disabled");
-       this.show();
-     }, 2000)
-   }
+    let loading = document.getElementById("spinner");
+    let loginButton = document.getElementById("loginButton");
+    loading!.hidden = false;
+    loginButton!.setAttribute("disabled", "disabled");
+    this.userService.login(this.formLogin.value)
+      .then(response => {
+        setTimeout(() => {
+          this.router.navigate(['/main'])
+          loading!.hidden = true;
+        }, 1000)
+      })
+    setTimeout(() => {
+      debugger
+      let input = document.getElementById("floatingInput");
+      let password = document.getElementById("floatingPassword");
+
+      input?.classList.add('ng-dirty');
+      password?.classList.add('ng-dirty');
+      loading!.hidden = true;
+      loginButton?.removeAttribute("disabled");
+      this.show();
+    }, 2000)
+  }
+
+  isFormInvalid(): boolean {
+    return this.formLogin.invalid || this.formLogin.get('email')!.value === '' || this.formLogin.get('password')!.value === '';
+  }
+}
+
+@NgModule({
+  imports: [
+    InputTextModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ToastModule,
+    TextBoxModule,
+    CommonModule,
+    PasswordModule
 
 
- }
 
- @NgModule({
-   imports: [
-     InputTextModule,
-     FormsModule,
-     ReactiveFormsModule,
-     ToastModule
 
-   ],
-   declarations: [LoginComponent],
-   exports: [LoginComponent]
- })
- export class LoginModule {
+  ],
+  declarations: [LoginComponent],
+  exports: [LoginComponent]
+})
+export class LoginModule {
 
- }
+}
 
 
